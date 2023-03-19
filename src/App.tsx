2b515bb381542c9247a624de5386c571ac763c76
wnortier <warwick.nortier@gmail.com>
2023-03-19 14:48:59 +0200

    replaced enum with tuple

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import useTsPaginator from 'ts-paginator';
+
+enum PaginationMessageVerb {
+  DISPLAYING = 'Displaying',
+  SHOWING = 'Showing',
+}
 
 function App() {
+  const { totalRecordCount, rowsPerPage, currentPage, _determinePaginationMessage, _determinePaginationDisabledState, _determineRowsPerPageOptions, _handleChangePage, _handleChangeRowsPerPage } = useTsPaginator(20, 10, 0);
+  const message = _determinePaginationMessage({ verb: PaginationMessageVerb.DISPLAYING });
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {message}
         </p>
         <a
           className="App-link"
