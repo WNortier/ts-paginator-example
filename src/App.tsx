3d277742e8b25bb5eca1d4f6b19ee9b3ef7004ed
wnortier <warwick.nortier@gmail.com>
2023-03-20 02:24:36 +0200

    updated example

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
-import React from "react";
+import useTsPaginator from 'ts-paginator'
 import './App.css';
-import useTsPaginator from "ts-paginator";
 
 function App() {
-  //@ts-ignore
+
   const {
     totalRecordCount,
     rowsPerPage,
     currentPage,
     _determinePaginationMessage,
-    _determinePaginationDisabledState,
     _determineRowsPerPageOptions,
+    _determinePaginationPages,
+    _determinePaginationDisabledState,
     _handleChangePage,
     _handleChangeRowsPerPage,
     _handleChangeTotalRecordCount
-  } = useTsPaginator(50, 0);
+  } = useTsPaginator(351, 15);
   const message = _determinePaginationMessage({ verb: "Showing" });
-  const rpp = _determineRowsPerPageOptions()
+  const rppo = _determineRowsPerPageOptions()
+  const pp = _determinePaginationPages()
   const disabled = _determinePaginationDisabledState()
+
   return (
     <div className="App">
       <header className="App-header">
@@ -27,12 +29,19 @@ function App() {
         </div>
         <p style={{ margin: '0px' }}>totalRecordCount: {totalRecordCount}</p>
         <p style={{ margin: '0px' }}>currentPage: {currentPage}</p>
+        <p style={{ margin: '0px' }}>rowsPerPage: {rowsPerPage}</p>
+        <hr style={{ width: '100%' }} />
         <p style={{ margin: '0px' }}>_determinePaginationMessage: {message}</p>
-        <p style={{ margin: '0px' }}>__determineRowsPerPageOptions: [{String(rpp).replace(/.{2}/g, '$&')}]</p>
+        <p style={{ margin: '0px' }}>_determineRowsPerPageOptions: [{String(rppo).replace(/.{2}/g, '$&')}]</p>
+        <p style={{ margin: '0px' }}>_determinePaginationPages: [{String(pp).replace(/.{2}/g, '$&')}]</p>
         <p style={{ margin: '0px' }}>_determinePaginationDisabledState: {String(disabled)}</p>
-        <button style={{ background: 'lightblue', fontSize: '16px', padding: '10px', borderRadius: '6px', margin: '20px', fontWeight: 'bold', cursor: 'pointer' }} onClick={() => _handleChangeTotalRecordCount(1000)} className="font">Set totalRecordCount to 1000</button>
-        <button style={{ background: 'lightblue', fontSize: '16px', padding: '10px', borderRadius: '6px', margin: '20px', fontWeight: 'bold', cursor: 'pointer' }} onClick={() => _handleChangeRowsPerPage(250)}>Change rowsPerPage to 250</button>
-        <button style={{ background: 'lightblue', fontSize: '16px', padding: '10px', borderRadius: '6px', margin: '20px', fontWeight: 'bold', cursor: 'pointer' }} onClick={() => _handleChangePage(4)}>Change to page 3</button>
+        <hr style={{ width: '100%' }} />
+        <div style={{ display: "flex" }}>
+          <button style={{ background: 'lightblue', fontSize: '24px', padding: '10px', borderRadius: '6px', margin: '20px', fontWeight: 'bold', cursor: 'pointer' }} onClick={() => _handleChangeTotalRecordCount(7)} className="font">Set totalRecordCount to 7</button>
+          <button style={{ background: 'lightblue', fontSize: '24px', padding: '10px', borderRadius: '6px', margin: '20px', fontWeight: 'bold', cursor: 'pointer' }} onClick={() => _handleChangeTotalRecordCount(1000)} className="font">Set totalRecordCount to 1000</button>
+          <button style={{ background: 'lightblue', fontSize: '24px', padding: '10px', borderRadius: '6px', margin: '20px', fontWeight: 'bold', cursor: 'pointer' }} onClick={() => _handleChangeRowsPerPage(250)}>Set rowsPerPage to 250</button>
+          <button style={{ background: 'lightblue', fontSize: '24px', padding: '10px', borderRadius: '6px', margin: '20px', fontWeight: 'bold', cursor: 'pointer' }} onClick={() => _handleChangePage(4)}>Set currentPage to page 3</button>
+        </div>
       </header>
     </div>
   );
